88be6f757644d6563399cfb333c61bb473e1d2b0
Nicholas Rodrigues <rodriguesnich@outlook.com>
2021-03-25 18:23:11 -0300

    v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, MouseEvent } from "react";
+import { evaluate } from "mathjs";
+import "./App.css";
+
 
 function App() {
+  const [calculation, SetCalculation] = useState('')
+  const [result, SetResult] = useState('')
+
+
+function Screen() {
+  return (
+    <div className="Screen">
+      <input type="text" value={calculation} />
+      <textarea value={result}></textarea>
+    </div>
+  );
+}
+
+function KeyBoard() {
+  
+  function handleClick(event: MouseEvent) {
+    console.log(event.currentTarget.textContent);
+    let buffer = calculation
+    SetCalculation(buffer += event.currentTarget.textContent)
+  }
+
+  function result() {
+    console.log(calculation);
+    let calc = String(calculation)
+    let result = evaluate(calc)
+    SetResult(result)
+  }
+
+  function clear() {
+    SetResult('')
+    SetCalculation('')
+  }
+
+  return (
+    <div className="Keyboard">
+      <div className="button-line">
+        <button onClick={clear}>ac</button>
+        <button>()</button>
+        <button>%</button>
+        <button onClick={handleClick}>/</button>
+      </div>
+      <div className="button-line">
+        <button onClick={handleClick}>7</button>
+        <button onClick={handleClick}>8</button>
+        <button onClick={handleClick}>9</button>
+        <button onClick={handleClick}>*</button>
+      </div>
+      <div className="button-line">
+        <button onClick={handleClick}>4</button>
+        <button onClick={handleClick}>5</button>
+        <button onClick={handleClick}>6</button>
+        <button onClick={handleClick}>-</button>
+      </div>
+      <div className="button-line">
+        <button onClick={handleClick}>1</button>
+        <button onClick={handleClick}>2</button>
+        <button onClick={handleClick}>3</button>
+        <button onClick={handleClick}>+</button>
+      </div>
+      <div className="button-line">
+        <button>r</button>
+        <button onClick={handleClick}>0</button>
+        <button onClick={handleClick}>.</button>
+        <button onClick={result}>=</button>
+      </div>
+    </div>
+  );
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Screen />
+      <KeyBoard />
     </div>
   );
 }
